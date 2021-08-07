32243c45b08c5c868c4efc8fc203828e7515c34d
OuJinJin <1016068816@qq.com>
2021-08-07 16:50:17 +0800

    feat(service):recruitmentInfoService

--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/Impl/RecruitmentInfoServiceImpl.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/Impl/RecruitmentInfoServiceImpl.java
@@ -1,16 +1,71 @@
 package com.oujiajun.recruitment.service.Impl;
 
 import com.oujiajun.recruitment.dao.RecruitmentInfoDao;
+import com.oujiajun.recruitment.entity.dto.ResultInfo;
+import com.oujiajun.recruitment.entity.po.RecruitmentInfo;
+import com.oujiajun.recruitment.service.RecruitmentInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author oujiajun
  */
 @Service
-public class RecruitmentInfoServiceImpl {
+public class RecruitmentInfoServiceImpl implements RecruitmentInfoService {
     @Autowired
     RecruitmentInfoDao recruitmentInfoDao;
 
+    @Override
+    public ResultInfo insertRecruitmentInfo(RecruitmentInfo recruitmentInfo) {
+        int count = recruitmentInfoDao.insertRecruitmentInfo(recruitmentInfo);
+        if (count >= 1){
+            return new ResultInfo(true);
+        }else {
+            return new ResultInfo(false);
+        }
+    }
 
+    @Override
+    public ResultInfo deleteRecruitmentInfo(int recruitmentInfoId) {
+        int count = recruitmentInfoDao.deleteRecruitmentInfo(recruitmentInfoId);
+        if (count >= 1){
+            return new ResultInfo(true);
+        }else {
+            return new ResultInfo(false);
+        }
+    }
+
+    @Override
+    public ResultInfo updateRecruitmentInfo(RecruitmentInfo recruitmentInfo) {
+        int count = recruitmentInfoDao.updateRecruitmentInfo(recruitmentInfo);
+        if (count >= 1){
+            return new ResultInfo(true);
+        }else {
+            return new ResultInfo(false);
+        }
+    }
+
+    @Override
+    public ResultInfo queryAllRecruitmentInfo() {
+        List<RecruitmentInfo> infoList = recruitmentInfoDao.queryAllRecruitmentInfo();
+        int number = infoList.size();
+        if (number >= 1){
+            return new ResultInfo(true,infoList);
+        }else {
+            return new ResultInfo(false);
+        }
+    }
+
+    @Override
+    public ResultInfo queryRecruitmentInfoById(int recruitmentInfoId) {
+        List<RecruitmentInfo> infoList = recruitmentInfoDao.queryRecruitmentInfoByUid(recruitmentInfoId);
+        int number = infoList.size();
+        if (number >= 1){
+            return new ResultInfo(true,infoList.get(0));
+        }else {
+            return new ResultInfo(false);
+        }
+    }
 }
--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/RecruitmentInfoService.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/RecruitmentInfoService.java
@@ -1,10 +1,44 @@
 package com.oujiajun.recruitment.service;
 
+import com.oujiajun.recruitment.entity.dto.ResultInfo;
+import com.oujiajun.recruitment.entity.po.RecruitmentInfo;
+
 /**
  * @author oujiajun
  */
 public interface RecruitmentInfoService {
 
-    
+    /**
+     * 增加招聘信息
+     * @param recruitmentInfo 招聘信息
+     * @return 服务结果集
+     */
+    ResultInfo insertRecruitmentInfo(RecruitmentInfo recruitmentInfo);
 
+    /**
+     * 删除招聘信息
+     * @param recruitmentInfoId 招聘信息id
+     * @return 服务结果集
+     */
+    ResultInfo deleteRecruitmentInfo(int recruitmentInfoId);
+
+    /**
+     * 修改招聘信息
+     * @param recruitmentInfo 招聘信息
+     * @return 服务结果集
+     */
+    ResultInfo updateRecruitmentInfo(RecruitmentInfo recruitmentInfo);
+
+    /**
+     * 查询所有招聘信息
+     * @return 服务结果集
+     */
+    ResultInfo queryAllRecruitmentInfo();
+
+    /**
+     * 根据招聘信息id查询招聘信息
+     * @param recruitmentInfoId 招聘信息id
+     * @return 服务结果集
+     */
+    ResultInfo queryRecruitmentInfoById(int recruitmentInfoId);
 }
