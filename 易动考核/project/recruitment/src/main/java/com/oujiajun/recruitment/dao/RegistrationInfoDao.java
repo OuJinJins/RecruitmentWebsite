cff2415ad8a70c9411178c0e59a1d0fa655dfd16
OuJinJin <1016068816@qq.com>
2021-08-09 06:55:13 +0800

    feat(dao):RegistrationInfoDao->crud

--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/dao/RegistrationInfoDao.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/dao/RegistrationInfoDao.java
@@ -1,10 +1,53 @@
 package com.oujiajun.recruitment.dao;
 
+import com.oujiajun.recruitment.entity.po.RegistrationInfo;
+
+import java.util.List;
+
 /**
  * @author oujiajun
  */
 public interface RegistrationInfoDao {
 
 
+    /**
+     * 增加报名信息
+     * @param registrationInfo 报名信息
+     * @return 修改行数
+     */
+    int insertRegistrationInfo(RegistrationInfo registrationInfo);
 
+    /**
+     * 删除报名信息
+     * @param registrationInfoId 报名信息id
+     * @return 修改行数
+     */
+    int deleteRegistrationInfo(Integer registrationInfoId);
+
+    /**
+     * 修改报名信息
+     * @param registrationInfo 报名信息
+     * @return 修改行数
+     */
+    int updateRegistrationInfo(RegistrationInfo registrationInfo);
+
+    /**
+     * 查询所有的报名信息
+     * @return 报名信息
+     */
+    List<RegistrationInfo> queryAllRegistrationInfo();
+
+    /**
+     * 根据报名信息id寻找报名信息
+     * @param registrationInfoId 报名信息id
+     * @return 报名信息
+     */
+    RegistrationInfo queryRegistrationInfoById(Integer registrationInfoId);
+
+    /**
+     * 通过报名者id查询报名信息
+     * @param userId 报名用户id
+     * @return 报名信息
+     */
+    List<RegistrationInfo> queryRegistrationInfoByUid(Integer userId);
 }
