6c2e2f40f7de16e033c050dd43e86701b7706703
OuJinJin <1016068816@qq.com>
2021-08-09 06:50:45 +0800

    feat(po):RegistrationInfo

--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/entity/po/RegistrationInfo.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/entity/po/RegistrationInfo.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 /**
  * @author oujiajun
  */
@@ -12,8 +15,44 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegistrationInfo {
 
+    /**
+     * 报名信息id
+     */
     Integer registrationInfoId;
 
+    /**
+     * 招聘信息id
+     */
+    Integer recruitmentInfoId;
 
+    /**
+     * 报名用户id
+     */
+    Integer userId;
+
+    /**
+     * 是否通过报名
+     */
+    Boolean isRegistrationPass;
+
+    /**
+     * 是否通过面试
+     */
+    Boolean isInterviewPass;
+
+    /**
+     * 面试日期
+     */
+    LocalDate interviewDate;
+
+    /**
+     * 面试时间段开始时间
+     */
+    LocalTime interviewTimeBegin;
+
+    /**
+     * 面试时间段结束时间
+     */
+    LocalTime interviewTimeEnd;
 
 }
