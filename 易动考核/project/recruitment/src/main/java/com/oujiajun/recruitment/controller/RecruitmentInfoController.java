6dd90230c0b77acbea2c95aeda74d34b82d7d50e
OuJinJin <1016068816@qq.com>
2021-08-11 03:49:39 +0800

    fix(recruitmentcontroller):解决选择日期时405的问题

--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/controller/RecruitmentInfoController.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/controller/RecruitmentInfoController.java
@@ -13,10 +13,7 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -285,7 +282,7 @@ public class RecruitmentInfoController {
         return "/chooseDate";
     }
 
-    @GetMapping("/interview/chooseDate")
+    @PostMapping("/interview/chooseDate")
     public String chooseDate(
             @RequestParam("interviewPeriodId")Integer interviewPeriodId,
             HttpServletRequest request,
