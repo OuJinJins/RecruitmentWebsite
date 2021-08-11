f3b3b4893877afd7499bd03ce41540aa36529e2f
OuJinJin <1016068816@qq.com>
2021-08-12 02:52:41 +0800

    feat(dao):queryUserRegistrationInfoByInterviewPeriodId

--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/dao/RegistrationInfoDao.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/dao/RegistrationInfoDao.java
@@ -88,6 +88,13 @@ public interface RegistrationInfoDao {
     List<UserRegistrationInfo> queryUserRegistrationInfoForInterviewer(@Param("recruitmentInfoId")Integer recruitmentInfoId);
 
     /**
+     * 寻找用户报名信息
+     * @param interviewPeriodId 时间段id
+     * @return 用户报名信息视图类
+     */
+    List<UserRegistrationInfo>  queryUserRegistrationInfoByInterviewPeriodId(@Param("interviewPeriodId")int interviewPeriodId);
+
+    /**
      * 插入面试时间报名信息
      * @param interviewPeriodId 面试时间段id
      * @param registrationInfoId 报名信息id
