e4100305992a8f5a49c24ee6b4e635cae893ef70
OuJinJin <1016068816@qq.com>
2021-08-17 21:36:03 +0800

    fix(service):权限和身份service

--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/Impl/PermissionServiceImpl.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/Impl/PermissionServiceImpl.java
@@ -6,12 +6,14 @@ import com.oujiajun.recruitment.entity.po.Permission;
 import com.oujiajun.recruitment.entity.po.Role;
 import com.oujiajun.recruitment.service.PermissionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * @author oujiajun
  */
+@Service
 public class PermissionServiceImpl implements PermissionService {
 
     @Autowired
--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/Impl/RoleServiceImpl.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/service/Impl/RoleServiceImpl.java
@@ -6,9 +6,11 @@ import com.oujiajun.recruitment.entity.po.RecruitmentInfo;
 import com.oujiajun.recruitment.entity.po.Role;
 import com.oujiajun.recruitment.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RoleServiceImpl implements RoleService {
 
     @Autowired
