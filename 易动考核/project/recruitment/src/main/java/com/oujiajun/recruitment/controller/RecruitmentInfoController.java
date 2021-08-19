9b843bfe0f5c39f6cca25348494706a9eda3fb03
OuJinJin <1016068816@qq.com>
2021-08-19 12:03:53 +0800

    fix():

--- 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/controller/RecruitmentInfoController.java
+++ 易动考核/project/recruitment/src/main/java/com/oujiajun/recruitment/controller/RecruitmentInfoController.java
@@ -136,20 +136,20 @@ public class RecruitmentInfoController {
      */
     @GetMapping("/interviewer/myRecruitment/detail/id/{recruitmentInfoId}")
     public String toMyDetailRecruitmentInfo(@PathVariable("recruitmentInfoId") Integer recruitmentInfoId,HttpServletRequest request,HttpSession session){
+        // 通过id查询招聘信息
         ResultInfo resultInfo = recruitmentInfoService.queryRecruitmentInfoById(recruitmentInfoId);
-        if (resultInfo.getSuccess()) {
-            RecruitmentInfo recruitmentInfo = (RecruitmentInfo) resultInfo.getData();
-            request.setAttribute("myRecruitmentInfo",recruitmentInfo);
-            ResultInfo userServiceInfo = userService.queryUserById(recruitmentInfo.getUserId());
-            if(userServiceInfo.getSuccess()){
-                User interviewer = (User)userServiceInfo.getData();
-                request.setAttribute("interviewer",interviewer);
-            }else {
-                session.setAttribute("errorMsg",userServiceInfo.getMessage());
-            }
-        }else {
+        if (!resultInfo.getSuccess()) {
             session.setAttribute("errorMsg",resultInfo.getMessage());
         }
+        RecruitmentInfo recruitmentInfo = (RecruitmentInfo) resultInfo.getData();
+        request.setAttribute("myRecruitmentInfo",recruitmentInfo);
+        // 查询招聘的用户信息
+        ResultInfo userServiceInfo = userService.queryUserById(recruitmentInfo.getUserId());
+        if(!userServiceInfo.getSuccess()){
+            session.setAttribute("errorMsg",userServiceInfo.getMessage());
+        }
+        User interviewer = (User)userServiceInfo.getData();
+        request.setAttribute("interviewer",interviewer);
         return "/interviewer/recruitmentDetail";
     }
 
@@ -180,32 +180,39 @@ public class RecruitmentInfoController {
         return "redirect:/interviewer/myRecruitment";
     }
 
+    /**
+     * 报名招聘
+     * @param recruitmentInfoId 招聘id
+     * @param session session
+     * @return 视图路径
+     */
     @GetMapping("/registrationInfo/signUp/id/{recruitmentInfoId}")
     public String signUp(@PathVariable("recruitmentInfoId") Integer recruitmentInfoId,HttpSession session){
-        ResultInfo recruitmentInfoServiceResult = recruitmentInfoService.queryRecruitmentInfoById(recruitmentInfoId);
-        if(recruitmentInfoServiceResult.getSuccess()){
-            RecruitmentInfo recruitmentInfo = (RecruitmentInfo)recruitmentInfoServiceResult.getData();
-            User loginUser = (User)session.getAttribute("loginUser");
-            RegistrationInfo registrationInfo = new RegistrationInfo();
-            registrationInfo.setRecruitmentInfoId(recruitmentInfoId);
-            registrationInfo.setUserId(loginUser.getId());
-            ResultInfo registrationInfoServiceResult = registrationInfoService.insertRegistrationInfo(registrationInfo);
-            if(!registrationInfoServiceResult.getSuccess()){
-                session.setAttribute("errorMsg",registrationInfoServiceResult.getMessage());
-            }
-        }else {
-            session.setAttribute("errorMsg",recruitmentInfoServiceResult.getMessage());
+        User loginUser = (User)session.getAttribute("loginUser");
+        RegistrationInfo registrationInfo = new RegistrationInfo();
+        registrationInfo.setRecruitmentInfoId(recruitmentInfoId);
+        registrationInfo.setUserId(loginUser.getId());
+        // 插入报名信息
+        ResultInfo registrationInfoServiceResult = registrationInfoService.insertRegistrationInfo(registrationInfo);
+        if(!registrationInfoServiceResult.getSuccess()){
+            session.setAttribute("errorMsg",registrationInfoServiceResult.getMessage());
         }
         return "redirect:/recruitment/detail/id/"+recruitmentInfoId;
     }
 
+    /**
+     * 前往我的报名
+     * @param request 请求
+     * @param session session
+     * @return 视图路径
+     */
     @GetMapping({ "/myRegistration","/myRegistration.html"})
     public String toMyRegistration(HttpServletRequest request, HttpSession session) {
         User loginUser = (User)session.getAttribute("loginUser");
         ResultInfo resultInfo = registrationInfoService.queryRegistrationInfoByUid(loginUser.getId());
         if(resultInfo.getSuccess()){
             List<RegistrationInfo> registrationInfoList = (List<RegistrationInfo>)resultInfo.getData();
-            List<RecruitmentInfo> recruitmentInfoList = new ArrayList<RecruitmentInfo>();
+            List<RecruitmentInfo> recruitmentInfoList = new ArrayList<>();
             for (RegistrationInfo registrationInfo : registrationInfoList) {
                 ResultInfo queryRecruitmentInfoResult = recruitmentInfoService.queryRecruitmentInfoById(registrationInfo.getRecruitmentInfoId());
                 if (queryRecruitmentInfoResult.getSuccess()){
@@ -221,6 +228,13 @@ public class RecruitmentInfoController {
         return "/myRegistration";
     }
 
+    /**
+     * 前往通过报名的面试者
+     * @param recruitmentInfoId 招聘信息id
+     * @param request 请求
+     * @param session session
+     * @return 试图路径
+     */
     @GetMapping({ "/registrationInfo/show/allInterviewees/id/{recruitmentInfoId}"})
     public String toInterviewees(@PathVariable("recruitmentInfoId")Integer recruitmentInfoId, HttpServletRequest request, HttpSession session) {
         User loginUser = (User)session.getAttribute("loginUser");
@@ -240,6 +254,13 @@ public class RecruitmentInfoController {
         return "/interviewer/interviewees";
     }
 
+    /**
+     * 前往展示该招聘信息所有报名者的视图
+     * @param recruitmentInfoId 招聘信息id
+     * @param request 请求
+     * @param session session
+     * @return /interviewer/applicants
+     */
     @GetMapping({ "/registrationInfo/show/allApplicants/id/{recruitmentInfoId}"})
     public String toApplicants(@PathVariable("recruitmentInfoId")Integer recruitmentInfoId, HttpServletRequest request, HttpSession session) {
         User loginUser = (User)session.getAttribute("loginUser");
@@ -258,11 +279,17 @@ public class RecruitmentInfoController {
         return "/interviewer/applicants";
     }
 
+    /**
+     * 通过报名
+     * @param recruitmentInfoId 招聘信息id
+     * @param registrationInfoId 报名信息id
+     * @param session session
+     * @return redirect:/registrationInfo/show/allApplicants/id/ + recruitmentInfoId
+     */
     @GetMapping("/interviewer/registration/pass/id/{recruitmentInfoId}/{registrationInfoId}")
     public String passRegistration(
-            @PathVariable("recruitmentInfoId")Integer recruitmentInfoId,
-            @PathVariable("registrationInfoId")Integer registrationInfoId,
-            HttpServletRequest request,
+            @PathVariable("recruitmentInfoId") Integer recruitmentInfoId,
+            @PathVariable("registrationInfoId") Integer registrationInfoId,
             HttpSession session){
         ResultInfo resultInfo = registrationInfoService.passRegistration(registrationInfoId);
         if(!resultInfo.getSuccess()){
@@ -273,16 +300,14 @@ public class RecruitmentInfoController {
 
     @GetMapping("/interviewer/registration/out/id/{recruitmentInfoId}/{registrationInfoId}")
     public String passOutRegistration(
-            @PathVariable("recruitmentInfoId")Integer recruitmentInfoId,
-            @PathVariable("registrationInfoId")Integer registrationInfoId,
-            HttpServletRequest request,
+            @PathVariable("recruitmentInfoId") Integer recruitmentInfoId,
+            @PathVariable("registrationInfoId") Integer registrationInfoId,
             HttpSession session){
         // 不通过报名审核
         ResultInfo resultInfo = registrationInfoService.passOutRegistration(registrationInfoId);
         if(!resultInfo.getSuccess()){
             session.setAttribute("errorMsg", resultInfo.getMessage());
         }
-        ResultInfo queryResult = registrationInfoService.queryRegistrationInfoById(registrationInfoId);
         return "redirect:/registrationInfo/show/allApplicants/id/" + recruitmentInfoId;
     }
 
@@ -393,6 +418,7 @@ public class RecruitmentInfoController {
                 userRegistrationInfoList.add(userRegistrationInfo);
             }
         }
+        request.setAttribute("registrationInfoId",registrationInfoId);
         request.setAttribute("beforeLineUpNumber",beforeLineUpNumber);
         return "/lineUp";
     }
@@ -495,9 +521,60 @@ public class RecruitmentInfoController {
             System.out.println(userRegistrationInfo);
             session.setAttribute("errorMsg","步过失败");
         }
-        System.out.println(userRegistrationInfoList);
         // userRegistrationInfoList
         return "redirect:/interview/startInterview/id/" + interviewPeriod.getInterviewPeriodId();
+    }
+
+    @GetMapping("/again/lineUp/pass/id/{registrationInfoId}")
+    public String lineUpAgain(
+            @PathVariable("registrationInfoId") Integer registrationInfoId,
+          HttpServletRequest request,
+          HttpSession session){
+        // 查询报名信息
+        ResultInfo queryUserRegistrationInfoResult = registrationInfoService.queryUserRegistrationInfoByRegistrationInfoId(registrationInfoId);
+        if (!queryUserRegistrationInfoResult.getSuccess()){
+            return "redirect:/index";
+        }
+        UserRegistrationInfo userRegistrationInfo = (UserRegistrationInfo) queryUserRegistrationInfoResult.getData();
+        // 查询此报名信息参加的面试时间段
+        ResultInfo queryInterviewPeriodResult = interviewPeriodService.queryInterviewPeriodByRegistrationInfoId(registrationInfoId);
+        if (!queryInterviewPeriodResult.getSuccess()){
+            session.setAttribute("errorMsg","步过失败");
+            return "redirect:/registrationInfo/start/interview/id/" + userRegistrationInfo.getRecruitmentInfoId();
+        }
+        InterviewPeriod interviewPeriod = (InterviewPeriod) queryInterviewPeriodResult.getData();
+        LinkedList<UserRegistrationInfo> userRegistrationInfoList = interviewPeriodListMap.get(interviewPeriod.getInterviewPeriodId());
+        // 从排队队伍中移除
+        if(!userRegistrationInfoList.remove(userRegistrationInfo)){
+            System.out.println(userRegistrationInfo);
+            session.setAttribute("errorMsg","步过失败");
+        }
+        // 进行排队
+        int beforeLineUpNumber;
+        if (userRegistrationInfoList == null){
+            // 创建一个新的
+            LinkedList<UserRegistrationInfo> userRegistrationInfos = new LinkedList<>();
+            userRegistrationInfos.add(userRegistrationInfo);
+            interviewPeriodListMap.put(interviewPeriod.getInterviewPeriodId(),userRegistrationInfos);
+            beforeLineUpNumber = 0;
+        }else {
+            if (userRegistrationInfoList.stream().anyMatch(userRegistrationInfo::equals)) {
+                beforeLineUpNumber = 0;
+                for (UserRegistrationInfo info : userRegistrationInfoList) {
+                    if(!info.equals(userRegistrationInfo)){
+                        beforeLineUpNumber++;
+                    }else {
+                        break;
+                    }
+                }
+            }else {
+                beforeLineUpNumber = userRegistrationInfoList.size();
+                userRegistrationInfoList.add(userRegistrationInfo);
+            }
+        }
+        request.setAttribute("registrationInfoId",registrationInfoId);
+        request.setAttribute("beforeLineUpNumber",beforeLineUpNumber);
+        return "/lineUp";
     }
 
     @GetMapping(value = {
